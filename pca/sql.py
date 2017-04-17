bba9bb9841ab7848af0e1dca2494465fe4f40ec9
Sumit Sohani <sumit_sohani@brown.edu>
2017-04-17 18:57:45 -0400

    Graph

--- pca/sql.py
+++ pca/sql.py
@@ -16,4 +16,21 @@ def write_data(filename):
         file_to_write.write(re.sub(rx, '', str(row)) + "\n")
     file_to_write.close()
 
-write_data('recent_compact_2013.csv')
+#write_data('recent_compact_2013.csv')
+
+def gni_graph(filename):
+    conn = sqlite3.connect('../database.sqlite')
+    c = conn.cursor()
+    file_to_write = open(filename, 'w')
+    rx = re.compile("\(|\)|'")
+    for row in c.execute(
+                        # SQL statement 
+                        """
+                            SELECT   CountryCode, IndicatorCode, Value 
+                            FROM     Indicators 
+                            WHERE    IndicatorCode="NY.GNP.PCAP.KD";
+                         """ ):
+        file_to_write.write(re.sub(rx, '', str(row)) + "\n")
+    file_to_write.close()
+
+gni_graph("gni_test_file.csv")
