2ae994199d6711ec6bc96ab2789a486fffcd307b
Sumit Sohani <sumit_sohani@brown.edu>
2017-05-08 22:45:42 -0400

    cluster analysis

--- sumit_sunday/cluster_analysis.py
+++ sumit_sunday/cluster_analysis.py
@@ -8,6 +8,8 @@ import statsmodels.formula.api as smf
 import sys
 import random
 import csv
+import networkx as nx
+import matplotlib.pyplot as plt
 
 folders = ["r_clusters", "hdi_cluster", "dr_cluster", "gni_cluster"]
 
@@ -33,15 +35,41 @@ def analyze(fName):
 		clusters[int(row["Cluster"])].append(row["Country"])
 		prev_yr = row["Year"]
 		print(prev_yr)
-	wtr = csv.writer(open("kmeans/" + fName + "/averages.csv", "w"))
-	wtr2 = csv.writer(open("kmeans/" + fName + "/similarities.csv", "w"))
-	wtr.writerow(["Country", "Average", "Stdev"])
-	wtr2.writerow(["Country", "Nbr1", "Nbr2", "Nbr3"])
+	#wtr = csv.writer(open("kmeans/" + fName + "/averages.csv", "w"))
+	#wtr2 = csv.writer(open("kmeans/" + fName + "/similarities.csv", "w"))
+	#wtr.writerow(["Country", "Average", "Stdev"])
+	#wtr2.writerow(["Country", "Nbr1", "Nbr2", "Nbr3"])
+	plt.figure()
+	plt.xlabel("Mean Cluster")
+	plt.ylabel("Standard Deviation")
+	plt.title("Avg Cluster Position vs Standard Deviation")
+	clstr = []
+	stdev = []
 	for c in countries:
-		wtr.writerow([c, np.mean(country_clusters[c]), np.std(country_clusters[c])])
-		nbrs = list(map(lambda x: x[0], similar_to_country[c].most_common(3)))
-		wtr2.writerow([c] + nbrs)
+		clstr.append(np.mean(country_clusters[c]))
+		stdev.append(np.std(country_clusters[c]))
+		
+		#wtr.writerow([c, np.mean(country_clusters[c]), np.std(country_clusters[c])])
+		#nbrs = list(map(lambda x: x[0], similar_to_country[c].most_common(3)))
+		#wtr2.writerow([c] + nbrs)
+	plt.plot(clstr, stdev, 'o')
+	plt.savefig("kmeans/" + fName + "/stdev_vs_mean")
+	'''
+	G=nx.Graph()
+	G.add_nodes_from(countries)
+	mc = Counter()
+	#labels = nx.draw_networkx_labels(G,pos=nx.spring_layout(G))
+	for c in countries:
+		nbrs = list(map(lambda x: x[0], similar_to_country[c].most_common(1)))
+		for n in nbrs:
+			mc[n] += 1
+			G.add_edge(c, n)
+	nx.draw_random(G)#, labels=labels)
+	plt.show()
+	print(mc.most_common(10))
+	'''
+#for f in folders:
+#	analyze(f)
 for f in folders:
 	analyze(f)
-
 
